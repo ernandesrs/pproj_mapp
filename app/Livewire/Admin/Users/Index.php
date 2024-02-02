de3f5f7fe00858e0558eb093853c9765131dcd5e
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-02-02 11:35:54 -0400

    Users list

--- app/Livewire/Admin/Users/Index.php
+++ app/Livewire/Admin/Users/Index.php
@@ -2,54 +2,69 @@
 
 namespace App\Livewire\Admin\Users;
 
-use App\Livewire\Helpers\Traits\AsListPage;
-use Livewire\Component;
+use App\Livewire\Admin\PageBases\PageListBase;
+use App\Models\User;
 
-class Index extends Component
+class Index extends PageListBase
 {
-    use AsListPage;
+    public $modelClass = User::class;
 
-    public function render()
-    {
-        return view('livewire..admin.users.index')
-            ->layout('livewire.admin.layout')
-            ->title($this->getLayoutTitle());
-    }
-
-    public function searchableFields()
-    {
-        return \App\Models\User::searchableFields;
-    }
-
-    public function getModelClass()
-    {
-        return \App\Models\User::class;
-    }
-
-    public function getPageTitle()
-    {
-        return __('admin/layout.users');
-    }
-
-    public function getPageBreadcrumbs()
+    function tableColumnData()
     {
         return [
             [
                 'label' => __('admin/layout.users'),
+                'key' => ['first_name', 'last_name']
+            ],
+            [
+                'label' => __('admin/worlds.username'),
+                'key' => 'username'
+            ],
+            [
+                'label' => __('admin/worlds.email'),
+                'key' => 'email'
+            ],
+            [
+                'label' => __('admin/worlds.create_date'),
+                'key' => 'created_at'
+            ]
+        ];
+    }
+
+    function searchableFields()
+    {
+        return ['first_name', 'last_name', 'username', 'email'];
+    }
+
+    function getPageActions()
+    {
+        return [
+            [
+                'text' => __('admin/worlds.new') . ' ' . __('admin/worlds.user'),
                 'href' => '#',
+                'icon' => 'plus-lg'
+            ]
+        ];
+    }
+
+    function getPageBreadcrumbs()
+    {
+        return [
+            [
+                'label' => __('admin/layout.users'),
+                'href' => route('admin.users.index'),
                 'disabled' => true
             ]
         ];
     }
 
-    public function getPageActions()
+    function getPageTitle()
     {
-        return [
-            [
-                'text' => __('admin/worlds.new') . ' ' . __('admin/worlds.user'),
-                'icon' => 'plus-lg',
-                'href' => '#'
-            ]
-        ];
+        return __('admin/layout.users');
+    }
+
+    function actionDelete()
+    {
+        return true;
     }
 }
